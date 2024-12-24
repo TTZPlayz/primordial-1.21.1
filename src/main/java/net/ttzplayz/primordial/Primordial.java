4b7ce71c1fd1bc59a219fd0f0e12fe37c536b90e
TTZPlayz <jr.jmcooper@gmail.com>
2024-12-23 23:28:02 -0800

    test commit

--- src/main/java/net/ttzplayz/primordial/Primordial.java
+++ src/main/java/net/ttzplayz/primordial/Primordial.java
@@ -8,7 +8,7 @@ import org.slf4j.LoggerFactory;
 public class Primordial implements ModInitializer {
 	public static final String MOD_ID = "primordial";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
-
+// test commit comment
 	@Override
 	public void onInitialize() {
 
