75795f4bb945b815c8aa2309f336c0a35423b6a1
Justin Cooper <justincooper@Justins-MacBook-Pro-2.local>
2024-12-23 23:23:25 -0800

    Initial commit

---
+++ src/main/java/net/ttzplayz/primordial/Primordial.java
@@ -0,0 +1,16 @@
+package net.ttzplayz.primordial;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Primordial implements ModInitializer {
+	public static final String MOD_ID = "primordial";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/net/ttzplayz/primordial/PrimordialClient.java
@@ -0,0 +1,10 @@
+package net.ttzplayz.primordial;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class PrimordialClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
